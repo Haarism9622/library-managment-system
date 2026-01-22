0ca5c288b81c2a259a701ec462feb171a255e322
Haaris Muhammad <muhammadhaaris8124@gmail.com>
2026-01-21 19:08:36 -0500

    Updated db.js file

--- server/db.js
+++ server/db.js
@@ -1,2 +1,12 @@
 // Connecting Node to PostgreSQL
 
+const { Pool } = require('pg');
+
+const pool = new Pool ({
+    user: 'haarism',
+    host: 'localhost',
+    database: 'library_db',
+    password: 'password',
+    port: 5432
+});
+
